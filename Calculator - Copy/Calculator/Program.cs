897768725e618a03f7e16ed0a59052b3e61aff7f
IAmIanBower <93166835+IAmIanBower@users.noreply.github.com>
2021-10-25 19:01:58 -0500

    Add files via upload

---
+++ Calculator - Copy/Calculator/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            string operation;
+
+            Console.WriteLine("Enter first number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Desired operation? Add, Subtract, Multiply, or Divide: ");
+            operation = Console.ReadLine();
+            if (operation == "Add")
+            {
+                AddNumbers(num1, num2);
+            }
+            else if (operation == "Subtract")
+            {
+                SubtractNumbers(num1, num2);
+            }
+            else if(operation == "Multiply")
+            {
+                MultiplyNumbers(num1, num2);
+            }
+            else if(operation == "Divide")
+            {
+                DivideNumbers(num1, num2);
+            }
+            else
+            {
+                Console.WriteLine("Error detected. Please run program again.");
+            }
+        }
+        static int AddNumbers(int num1, int num2)
+        {
+            int Result = num1 + num2;
+            Console.WriteLine(Result);
+            return Result;
+        }
+        static int SubtractNumbers(int num1, int num2)
+        {
+            int Result = num1 - num2;
+            Console.WriteLine(Result);
+            return Result;
+        }
+        static int MultiplyNumbers(int num1, int num2)
+        {
+            int Result = num1 * num2;
+            Console.WriteLine(Result);
+            return Result;
+        }
+        static int DivideNumbers(int num1, int num2)
+        {
+            int Result = num1 / num2;
+            Console.WriteLine(Result);
+            return Result;
+        }
+    }
+}
